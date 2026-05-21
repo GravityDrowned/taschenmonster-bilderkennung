4d3b3b2d8c07b08b6968387e8b0c92ef2fb4cca9
michaelwagner <michael.wagner@codecentric.de>
2026-05-21 20:47:08 +0200

    feat: pytesseract text ocr

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import numpy as np
 import cv2
+import pytesseract
+from matplotlib import pyplot as plt
+
+def get_text_from_frame(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #extracted_text = pytesseract.image_to_string(gray, 'eng')
+    data = pytesseract.image_to_data(gray, lang='eng', output_type=pytesseract.Output.DICT)
+
+    n_boxes = len(data['level'])
+    for i in range(n_boxes):
+        (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    plt.figure(figsize=(10, 6))
+    plt.imshow(img)
+    plt.title("Image with Text Bounding Boxes")
+    plt.axis("off")
+    plt.show()
 
 def read_video(path):
     cap = cv2.VideoCapture(path)
@@ -11,6 +29,9 @@ def read_video(path):
         ret, frame = cap.read()
         if ret:
             cv2.imshow('Frame', frame)
+
+            text = get_text_from_frame(frame)
+
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
