744558e70681a0a25a59ea0e4c855a5693cd26d3
michaelwagner <michael.wagner@codecentric.de>
2026-05-25 14:21:12 +0200

    fix: run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir -p /run/systemd/system/bluetooth.service.d
 cat > /run/systemd/system/bluetooth.service.d/nxbt.conf << EOF
 [Service]
 ExecStart=
-ExecStart=/usr/libexec/bluetooth/bluetoothd --compat --noplugin=*
+ExecStart=/usr/sbin/bluetoothd --compat --noplugin=*
 EOF
 systemctl daemon-reload
 systemctl restart bluetooth
